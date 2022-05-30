2a13430329883082d0ce9166505819abb85bf90e
Chengjie Huang <chuang44@sheffield.ac.uk>
2022-05-30 22:39:44 +0100

    remove a redundant line

--- apps/webget.cc
+++ apps/webget.cc
@@ -8,8 +8,7 @@ using namespace std;
 
 void get_URL(const string &host, const string &path) {
     // Your code here.
-    const uint16_t portnum = ((std::random_device()()) % 50000) + 1025;
-
+    
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
     // then request the URL path given in the "path" string.
